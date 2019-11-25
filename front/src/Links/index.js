9b7b94cc57b4f295a235314d0722488b582a4459
Vterebenin <terebenin.work@gmail.com>
2019-11-25 21:44:32 +0300

    feat: prepare front

---
+++ front/src/Links/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+       there will be a links
+      </div>
+    );
+  }
+}
+
+export default Index;
